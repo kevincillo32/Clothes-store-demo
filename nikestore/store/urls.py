0e255886f3b16e1e2274cf4ffe4aa95958018d7c
Christian Hernández <86026709+Christ1anH@users.noreply.github.com>
2022-11-14 13:29:35 -0600

    Update urls.py

--- nikestore/store/urls.py
+++ nikestore/store/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('zapatos', views.zapatos, name='zapatos'),
     path('mochilas', views.mochilas, name='mochilas'),
     path('balones', views.balones, name='balones'),
+    path('camisagris', views.camisagris, name='camisagris'),
+    path('camisablanca', views.camisablanca, name='camisablanca'),
+    path('camisablanca2', views.camisablanca2, name='camisablanca2'),
+    path('camisanegra', views.camisanegra, name='camisanegra'),
+    path('camisaazul', views.camisaazul, name='camisaazul'),
+    path('camisaroja', views.camisaroja, name='camisaroja'),
 ]
