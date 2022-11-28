c0c638b3de3ace1b5349a9813c660e0c7bf80e1f
Kevin Carballo Herrera <65192229+kevincillo32@users.noreply.github.com>
2022-11-27 21:19:18 -0600

    Add files via upload

---
+++ authentication/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib.auth.models import User, auth
+from django.views.decorators.csrf import csrf_exempt
+
+
+
+# Create your views here.
+def login(request):
+    return render(request, "login.html")
+
+def registrarse(request):
+    if request.method == ("POST"):
+        first_name= request.POST['first_name']
+        last_name= request.POST['last_name']
+        email= request.POST['email']
+        password= request.POST['password']
+        user = User.objects.create_user(username=email, first_name=first_name, last_name=last_name, email=email, password=password)
+        user.save();
+        print("Usuario Creado")
+        redirect('/')
+        return render(request,'registrarse.html')
+    else:
+        
+        print("Intentalo de nuevo")
+        return render(request,'registrarse.html')
